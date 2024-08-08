1f87cc7563376d8fbebca3df6ca96ef20ae82023
Vitaly367 <vya367@gmail.com>
2024-08-09 00:00:59 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+# 1st program
+print(9**0.5*5)
+
+# 2st program
+print(9.99>9.98 and 1000!=1000.1)
+
+# 3st program
+print(2*2+2)
+print(2*(2+2))
+print((2*2+2)==(2*(2+2)))
+
+# 4st program
+'123.456'
+item = '123.456'
+print(item[ -3 ])
